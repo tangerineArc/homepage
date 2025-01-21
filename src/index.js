ac2fcbc121f9f71f4ecd953e5bb743f5ad5db3d7
Swagatam Pati <87703965+tangerineArc@users.noreply.github.com>
2025-01-21 16:56:23 +0530

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,5 @@
+"use strict";
+
+import "./style.css";
+
+console.log("Goddess of Dawn");
---
+++ webpack.common.js
@@ -0,0 +1,34 @@
+import path from "path";
+import { fileURLToPath } from "url";
+import HtmlWebpackPlugin from "html-webpack-plugin";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+export const common = {
+  entry: {
+    app: "./src/index.js",
+  },
+  plugins: [new HtmlWebpackPlugin({ template: "./src/template.html" })],
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
+    clean: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.html$/i,
+        loader: "html-loader",
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif|webp|mp3)$/i,
+        type: "asset/resource",
+      },
+    ],
+  },
+};
